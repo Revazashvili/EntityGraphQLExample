6670fea241f3aceb9d078850030299cb4c598d5a
revazashvili <levanrevazashvili@gmail.com>
2022-08-07 10:50:38 +0400

    fix pipeline

--- src/EntityGraphQLExample/Program.cs
+++ src/EntityGraphQLExample/Program.cs
@@ -25,8 +25,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 app.UseRouting();
+app.UseAuthorization();
 app.UseEndpoints(routeBuilder =>
 {
     routeBuilder.MapControllers();
