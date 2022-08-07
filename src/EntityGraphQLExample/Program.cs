5a1e8927166f2c75bab9fa3595b1f412e121ae4c
revazashvili <levanrevazashvili@gmail.com>
2022-08-07 14:17:09 +0400

    remove controllers

--- src/EntityGraphQLExample/Program.cs
+++ src/EntityGraphQLExample/Program.cs
@@ -6,32 +6,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddDbContext<CibContext>(opt => opt.UseInMemoryDatabase("cib"));
 builder.Services.AddGraphQLSchema(Schema.AddGraphQlOptions);
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-var cibContext = app.Services.CreateScope().ServiceProvider.GetRequiredService<CibContext>();
+await using var serviceScope = app.Services.CreateAsyncScope();
+var cibContext = serviceScope.ServiceProvider.GetRequiredService<CibContext>();
 await DatabaseSeeder.SeedAsync(cibContext);
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
-app.UseAuthorization();
 app.UseEndpoints(routeBuilder =>
 {
-    routeBuilder.MapControllers();
     routeBuilder.MapGraphQL<CibContext>();
     routeBuilder.MapGraphQLAltair(new AltairOptions
     {
